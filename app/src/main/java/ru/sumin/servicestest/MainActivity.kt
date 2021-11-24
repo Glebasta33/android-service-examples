ddce0dc8a719ef20579bce42ed8c8986870fefbf
Glebasta33 <gleb.trusow@yandex.ru>
2021-11-24 10:35:01 +0300

    add MyJobService

--- app/src/main/java/ru/sumin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -3,6 +3,9 @@ package ru.sumin.servicestest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -31,6 +34,17 @@ class MainActivity : AppCompatActivity() {
         }
         binding.intentService.setOnClickListener {
             startService(MyIntentService.newIntent(this))
+        }
+        binding.jobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+            jobScheduler.schedule(jobInfo)
         }
     }
 }
